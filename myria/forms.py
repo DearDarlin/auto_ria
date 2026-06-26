6e1f2c9b432d0f16e8e5a3e7e2a9ad3bccc98999
DearDarlin <darinakovalenko78@gmail.com>
2026-06-27 00:28:19 +0300

    text choose in add car page

--- myria/forms.py
+++ myria/forms.py
@@ -4,11 +4,11 @@ import datetime
 
 def get_years_choices():
     current_year = datetime.date.today().year
-    years =[(str(y), str(y)) for y in range(current_year, 1899, -1)]
+    years = [(str(y), str(y)) for y in range(current_year, 1899, -1)]
     return [('', 'Оберіть')] + years
 
 class CarForm(forms.ModelForm):
-    ransport_type = forms.ChoiceField(
+    transport_type = forms.ChoiceField(
         choices=[], 
         widget=forms.Select(attrs={'class': 'form-select'})
     )
@@ -20,14 +20,14 @@ class CarForm(forms.ModelForm):
             'min': '0',       
             'step': '1',      
             'onkeypress': 'return event.charCode >= 48 && event.charCode <= 57'
-        }))
+        })
+    )
     
     class Meta:
         model = Car
         exclude = ['seller', 'main_photo']
         
         widgets = {
-            
             'region': forms.Select(attrs={'class': 'form-select'}),
             'imported_from': forms.Select(attrs={'class': 'form-select'}),
             'accident': forms.Select(attrs={'class': 'form-select'}),
@@ -44,22 +44,23 @@ class CarForm(forms.ModelForm):
             'seat_heating': forms.Select(attrs={'class': 'form-select'}),
             'seat_ventilation': forms.Select(attrs={'class': 'form-select'}),
             'currency': forms.Select(attrs={'class': 'form-select'}),
-            'year': forms.Select(
-               choices=get_years_choices(),
-            ),
-            
-  
+            'year': forms.Select(choices=get_years_choices()),
         }
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         
         from .models import Brand
-        
         existing_types = Brand.objects.values_list('transport_type', flat=True).distinct()
         dynamic_choices = [('', 'Оберіть')] + [(t, t) for t in existing_types if t]
         self.fields['transport_type'].choices = dynamic_choices
-        if 'brand' in self.fields:
-            self.fields['brand'].empty_label = "Оберіть"
-        if 'model_name' in self.fields:
-            self.fields['model_name'].empty_label = "Оберіть"
+        
+        for field_name, field in self.fields.items():
+            if isinstance(field, forms.ModelChoiceField):
+                field.empty_label = "Оберіть"
+            
+            elif isinstance(field, forms.ChoiceField) and field_name != 'transport_type' and field_name != 'year':
+                if field.choices and field.choices[0][0] == '':
+                    current_choices = list(field.choices)
+                    current_choices[0] = ('', 'Оберіть')
+                    field.choices = current_choices

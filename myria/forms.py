1a227d2524a81109de9072198a0e477783e80cfb
DearDarlin <darinakovalenko78@gmail.com>
2026-06-26 20:43:22 +0300

    years and text choose

--- myria/forms.py
+++ myria/forms.py
@@ -1,13 +1,26 @@
 from django import forms
 from .models import Car
+import datetime
+
+def get_years_choices():
+    current_year = datetime.date.today().year
+    years =[(str(y), str(y)) for y in range(current_year, 1899, -1)]
+    return [('', 'Оберіть')] + years
+
+
 
 class CarForm(forms.ModelForm):
+    ransport_type = forms.ChoiceField(
+        choices=[], 
+        widget=forms.Select(attrs={'class': 'form-select'})
+    )
+    
     class Meta:
         model = Car
         exclude = ['seller', 'main_photo']
         
         widgets = {
-            'transport_type': forms.Select(attrs={'class': 'form-select'}),
+            
             'region': forms.Select(attrs={'class': 'form-select'}),
             'imported_from': forms.Select(attrs={'class': 'form-select'}),
             'accident': forms.Select(attrs={'class': 'form-select'}),
@@ -24,4 +37,21 @@ class CarForm(forms.ModelForm):
             'seat_heating': forms.Select(attrs={'class': 'form-select'}),
             'seat_ventilation': forms.Select(attrs={'class': 'form-select'}),
             'currency': forms.Select(attrs={'class': 'form-select'}),
+            'year': forms.Select(
+               choices=get_years_choices(),
+            ),
+  
         }
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        
+        from .models import Brand
+        
+        existing_types = Brand.objects.values_list('transport_type', flat=True).distinct()
+        dynamic_choices = [('', 'Оберіть')] + [(t, t) for t in existing_types if t]
+        self.fields['transport_type'].choices = dynamic_choices
+        if 'brand' in self.fields:
+            self.fields['brand'].empty_label = "Оберіть"
+        if 'model_name' in self.fields:
+            self.fields['model_name'].empty_label = "Оберіть"
